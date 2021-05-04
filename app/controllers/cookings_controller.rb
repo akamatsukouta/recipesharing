7d08fbc9556dc97ac43a4f1020e7bda2737f5a45
akamatsukouta <kouta0618_5fam2000@icloud.com>
2021-05-04 19:58:14 +0900

    newアクションとcreateアクション記述

--- app/controllers/cookings_controller.rb
+++ app/controllers/cookings_controller.rb
@@ -1,4 +1,19 @@
 class CookingsController < ApplicationController
   def index
+    @cookings = Cooking.order("created_at DESC")
+  end
+
+  def new
+    @cooking = Cooking.new
+  end
+
+  def create
+    @cooking = Cooking.new(cooking_params)
+    @cooking.user_id = current_user.id
+    if @cooking.save
+      redirect_to root_path   
+    else
+      render :new
+    end
   end
 end
