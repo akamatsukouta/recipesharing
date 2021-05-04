d52d3288aa863024b9ef78d299e2b194f2f7c0a2
akamatsukouta <kouta0618_5fam2000@icloud.com>
2021-05-04 20:15:35 +0900

    プライベートメソッドでストロングパラメーター記述

--- app/controllers/cookings_controller.rb
+++ app/controllers/cookings_controller.rb
@@ -16,4 +16,10 @@ class CookingsController < ApplicationController
       render :new
     end
   end
+
+  private
+  def cooking_params
+    params.require(:cooking).permit(:cooking_name, :material, :recipe, :point1, :point2, :point3, :production_time, images:[]).merge(user_id: current_user.id)
+  end
+  
 end
