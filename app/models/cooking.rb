d356f1949ccfbd07ac2ec40fa9b6df966171b0cb
akamatsukouta <kouta0618_5fam2000@icloud.com>
2021-05-04 20:12:45 +0900

    cookingモデルにバリデーション記述

--- app/models/cooking.rb
+++ app/models/cooking.rb
@@ -3,4 +3,16 @@ class Cooking < ApplicationRecord
   belongs_to :user
   has_many :likes, dependent: :destroy
   has_many_attached :images
+
+  validates :cooking_name, presence: true
+  validates :recipe,       presence: true
+  validates :material,     presence: true
+  validates :production_time, presence: true
+  validate :image_length
+
+  def image_length
+    if images.length >= 5
+      errors.add(:images, "4枚以内にしてください")
+    end
+  end
 end
