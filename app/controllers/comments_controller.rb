6a8ba597a2be2141137a6019ec38400504dab3f8
akamatsukouta <kouta0618_5fam2000@icloud.com>
2021-05-05 13:28:19 +0900

    createアクションとshowアクション追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,16 @@
 class CommentsController < ApplicationController
   def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to "/cookings/#{@comment.cooking.id}"
+    else
+      render "cookings/show"
+    end
   end
 
   def show
+    @comments = Comment.all
   end
+
+  
 end
