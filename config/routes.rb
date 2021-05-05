eac69cbb30d1bd02761016cb639757b76b2da0e4
akamatsukouta <kouta0618_5fam2000@icloud.com>
2021-05-05 12:44:03 +0900

    destroyアクションへのルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'cookings#index'
-  resources :cookings, only: [:new, :create, :show, :edit, :update]
+  resources :cookings, only: [:new, :create, :show, :edit, :update, :destroy]
 end
