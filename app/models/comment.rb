49b4307e692a55c77c1f7f51f1aa36433d5f68f0
akamatsukouta <kouta0618_5fam2000@icloud.com>
2021-05-05 13:34:19 +0900

    commentモデルにアソシエーションとバリデーション記述

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,8 @@
 class Comment < ApplicationRecord
+  belongs_to :user
+  belongs_to :cooking
+
+  validates :text, presence: true
+  validates :user, presence: true
+  validates :cooking, presence: true
 end
